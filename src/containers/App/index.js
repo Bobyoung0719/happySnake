0abc449c7e1b9fd059ed9fadc7866263532f4d8c
杨涛 <yangtao@hujiang.com>
2017-08-28 14:59:24 +0800

    preson-info

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+const App = () => (
+  <div className="app-container">
+    <div className="app-header">
+    </div>
+    <p className="main-content">
+    
+    </p>
+  </div>
+);
+
+export default App;
